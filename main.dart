3758d1278520b1d0120c1631e0ce42bf83bc0558
Marcos Henrique Ribeiro de Santana. <161729448+MarcosHenriqueRS10@users.noreply.github.com>
2025-05-19 15:38:18 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -5,37 +5,51 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    const title = 'Basic List';
+    const title = 'Contacts';
     List<Contato> contatos = [
-  Contato("Marcos","marcos@gmail.com"),
-  Contato("vini","vini@gmail.com"),
-  Contato("rafa","rafa@gmail.com"),
-  Contato("guilherme","guilherme@gmail.com"),
-  Contato("badu","badu@gmail.com"),
-];
+      Contato("Alma Christensen", "alma.christensen@example.com"),
+      Contato("Sergio Hill", "sergio.hill@example.com"),
+      Contato("Malo Gonzalez", "malo.gonzalez@example.com"),
+      Contato("Miguel Owens", "miguel.owens@example.com"),
+      Contato("Lilou Dumont", "lilou.dumont@example.com"),
+      Contato("Ashley Stewart", "ashley.stewart@example.com"),
+      Contato("Roman Zhang", "roman.zhang@example.com"),
+    ];
 
     return MaterialApp(
       title: title,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: const TextTheme(
+          titleMedium: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text(title),
         ),
         body: ListView.builder(
-          itemCount:contatos.length,
-          itemBuilder: (context, index){
-          return ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Color.fromARGB(255, 3, 26, 228),
-              child: Text('${contatos[index].nomeCompleto?[0].toUpperCase()}', style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),),
-            ),
-            title: Text('${contatos[index].nomeCompleto}'),
-            subtitle: Text('${contatos[index].email}') ,
-          );
-           }, 
-        ),      ),
+          itemCount: contatos.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.blue,
+                child: Text(
+                  contatos[index].nomeCompleto![0].toUpperCase(),
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+              title: Text(
+                contatos[index].nomeCompleto!,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(contatos[index].email!),
+            );
+          },
+        ),
+      ),
     );
   }
 }
@@ -43,10 +57,6 @@ class MyApp extends StatelessWidget {
 class Contato {
   String? nomeCompleto;
   String? email;
-  Contato(this.nomeCompleto,this.email){}
 
-
-
+  Contato(this.nomeCompleto, this.email);
 }
- 
- 
